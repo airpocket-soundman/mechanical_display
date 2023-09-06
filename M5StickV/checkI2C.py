6e1ddcf376f7b6ab28b521916fe2ede383625b19
airpocket <yamashita.3154@gmail.com>
2023-09-07 07:15:23 +0900

    iwc

--- M5StickV/checkI2C.py
+++ M5StickV/checkI2C.py
@@ -10,8 +10,12 @@ import lcd
 import sensor
 
 #I2C　初期化
-i2c = I2C(I2C.I2C0, freq=100000, scl=34, sda=35)
 
+
+scl_pin = GPIO(34, GPIO.PULL_NONE)
+sda_pin = GPIO(35, GPIO.PULL_NONE)
+i2c = I2C(I2C.I2C0, freq=100000, scl=34, sda=35)
+#i2c = I2C(I2C.I2C0, freq=100000, scl=34, sda=35)
 #I2C 接続されているユニットのアドレス確認
 addr = i2c.scan()
 print( "address is :" + str(addr) )
